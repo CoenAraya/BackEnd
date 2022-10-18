5148a0a3e04d404de22c90daf81acab90f22216d
CoenAraya <102705035+CoenAraya@users.noreply.github.com>
2022-10-18 01:33:38 -0300

    segunda entrega

---
+++ test.js
@@ -0,0 +1,34 @@
+const Contenedor = require('./contenedor.js')
+
+const nuevoProducto = new Contenedor('productos.txt')
+
+const testEjercicio = async () => {
+
+    //agregando objetos al archivo
+    await nuevoProducto.save( {titulo: 'El gran libro', precio: 200}),
+    await nuevoProducto.save( {titulo: 'Las ventajas de ser invisible', precio: 5100}),
+    await nuevoProducto.save( {titulo: 'Mas claro que la luz', precio: 700}),
+    await nuevoProducto.save( {titulo: 'El sigle', precio: 1200})
+    
+    //muestra un objeto segun el id
+    const prodID = await nuevoProducto.getById(2);
+    console.log(prodID)
+
+
+
+    //mostrar array
+    const todos = await nuevoProducto.getAll();
+    console.log(todos)
+
+
+
+    await nuevoProducto.deleteById(1)
+
+    // borrar todo (desactivaractivar)
+    // /await nuevoProducto.deleteAll()
+
+}
+
+
+testEjercicio()
+
